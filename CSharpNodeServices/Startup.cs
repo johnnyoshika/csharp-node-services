6c5819955a7fede4886dd0efec43c85e7d8e0ffa
Johnny Oshika <johnnyoshika@gmail.com>
2019-11-18 17:39:48 -0800

    Migrate to Jering.Javascript.NodeJS

--- CSharpNodeServices/Startup.cs
+++ CSharpNodeServices/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Jering.Javascript.NodeJS;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -17,7 +18,7 @@ namespace CSharpNodeServices
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddNodeServices();
+            services.AddNodeJS();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
